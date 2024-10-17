ea399f3978a7721c8ca3c5818202640d36d392bd
Andreas <andreas.pi.johansson@gmail.com>
2024-10-17 10:49:45 +0200

    all the stuff

--- V42/script.js
+++ V42/script.js
@@ -4,8 +4,27 @@ const totalSum =  document.getElementById("totalSum");
 const dricksAmount =  document.getElementById("dricksAmount");
 const paymentWrapper =  document.getElementById("paymentWrapper");
 const submitButton = document.querySelector("button")
+const payAmount = document.querySelector("#payAmount")
+const charityAmount = document.querySelector("#charityAmount")
+
 
 submitButton.addEventListener("click", function (clickEvent) {
-    clickEvent.preventDefault()
+    clickEvent.preventDefault();
+
+    // const amountPerPerson = Math.ceil((totalSum.value * (dricksAmount.value / 100 + 1))/ friendsAmount.value);
+    const amountPerPerson = calcAmountPerPerson(totalSum.value, dricksAmount.value, friendsAmount.value)
+
+    const charityValue = (amountPerPerson * friendsAmount.value) - (totalSum.value * (dricksAmount.value / 100 + 1))
     
+    console.log(amountPerPerson, charityValue)
+
+    form.style.display = "none";
+    paymentWrapper.style.display = "flex"
+
+    payAmount.textContent = `${amountPerPerson} kr`;
+    charityAmount.textContent = `${charityValue} kr`;
 })
+
+function calcAmountPerPerson(totalSum, dricksAmount, friendsAmount) {
+    return Math.ceil((totalSum * (dricksAmount / 100 + 1))/ friendsAmount);
+}
