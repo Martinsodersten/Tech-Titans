87ee8676a67fd64a89ee4d747edfcc858068ddec
Andreas <andreas.pi.johansson@gmail.com>
2024-10-17 10:06:15 +0200

    V42 exercises

---
+++ V42/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const friendsAmount =  document.getElementById("friendsAmount");
+const totalSum =  document.getElementById("totalSum");
+const dricksAmount =  document.getElementById("dricksAmount");
+const paymentWrapper =  document.getElementById("paymentWrapper");
+const submitButton = document.querySelector("button")
+
+submitButton.addEventListener("click", function (clickEvent) {
+    clickEvent.preventDefault()
+    
+})
