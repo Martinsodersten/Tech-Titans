9c224067b973ff105d15531ee7c1553b6c423c0e
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-13 11:26:50 +0100

    fix broken quiz buttons

--- v7/react-quiz/src/app/quiz/page.jsx
+++ v7/react-quiz/src/app/quiz/page.jsx
@@ -3,6 +3,8 @@
 import { useContext, useState } from "react";
 import { QuizContext } from "@/context/quizContext";
 
+import Link from 'next/link'
+
 export default function QuizPage() {
   const context = useContext(QuizContext);
   const { questions } = context;
@@ -25,20 +27,24 @@ export default function QuizPage() {
 
   return (
     <section className="w-full px-8 h-3/5 mt-12 flex flex-col justify-between">
-      <h1 className="text-center text-lg font-bold w-full">
-        {questions[currentQuestion].question}
-      </h1>
     
       {currentQuestion < questions.length
-      ? <div className="w-full h-32 grid grid-cols-2 gap-1">
+      ? <>
+          <h1 className="text-center text-lg font-bold w-full">
+            {questions[currentQuestion].question}
+          </h1>
+          <div className="w-full h-32 grid grid-cols-2 gap-1">
           {questions[currentQuestion].answers.map((answer, index) =>
-            <button key={index} onClick={() => setSelectedAnswer(answer)} className="w-full hover:opacity-90 rounded-md text-xs text-white font-medium border-b-4 bg-red-500 border-red-600">
+            <button key={index} onClick={() => handleNextQuestion(selectedAnswer)} className="w-full hover:opacity-90 rounded-md text-xs text-white font-medium border-b-4 bg-red-500 border-red-600">
               {answer}
             </button>
           )}
-          <button onClick={() => handleNextQuestion(selectedAnswer)}>Next question</button>
         </div>
-      : <p>Färdigt!</p>
+      </>
+      : <>
+          <h1>Du fick {points} poäng!</h1>
+          <Link href="/"><button>Tillbaka till start</button></Link>
+      </>
     }
     </section>
   )
