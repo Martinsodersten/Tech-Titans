6fef56cb7ecd29fb61def92e80a386521d1f9b91
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-13 11:05:37 +0100

    quiz

--- v7/react-quiz/src/app/admin/page.jsx
+++ v7/react-quiz/src/app/admin/page.jsx
@@ -1,9 +1,24 @@
+"use client";
 
+import { useContext, useState } from "react";
+import { QuizContext } from "@/context/quizContext";
 
 export default function AdminPage() {
+  const context = useContext(QuizContext);
+  const { questions } = context;
+
   return (
-    <>
+    <main>
       <h1>Admin</h1>
-    </>
+      <form>
+        {questions.map(question => 
+        <p>
+          <input type="text" value={question.question} />
+          {question.answers.map(answer => <input type="text" value={answer} />)}
+        </p>
+        )}
+        <button >Spara quiz</button>
+      </form>
+    </main>
   )
 }
--- v7/react-quiz/src/app/quiz/page.jsx
+++ v7/react-quiz/src/app/quiz/page.jsx
@@ -1,10 +1,51 @@
+"use client";
 
-
+import { useContext, useState } from "react";
+import { QuizContext } from "@/context/quizContext";
 
 export default function QuizPage() {
+  const context = useContext(QuizContext);
+  const { questions } = context;
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  
+  const [points, setPoints] = useState(0);
+
+  function handleNextQuestion(selectedAnswer) {
+    if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+      setPoints(prevPoints => prevPoints + 1);
+    }
+
+    if (currentQuestion < questions.length) {
+      setCurrentQuestion(prevQuestion => prevQuestion + 1);
+      setSelectedAnswer(null);
+    }
+  }
+
   return (
-    <>
-      <h1>Quiz</h1>
-    </>
+    <main>
+      <h1>Quiz - {points} poäng</h1>
+      {currentQuestion < questions.length
+      ? <>
+          {questions[currentQuestion].question}
+          {questions[currentQuestion].answers.map((answer, index) =>
+          <div key={index}>
+            <input
+              type="radio"
+              id={`answer${index}`}
+              name="answer"
+              value={answer}
+              onChange={() => setSelectedAnswer(answer)}
+            />
+            <label htmlFor={`answer${index}`}>{answer}</label>
+          </div>
+          )}
+          <button onClick={() => handleNextQuestion(selectedAnswer)}>Next question</button>
+        </>
+      : <p>Färdigt!</p>
+    }
+      
+    </main>
   )
 }
