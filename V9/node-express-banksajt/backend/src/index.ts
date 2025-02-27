76a59de203ba9c5fe70275e07c70e855e5fd93d5
Andreas <andreas.pi.johansson@gmail.com>
2025-02-27 11:47:36 +0100

    added db connection

--- V9/node-express-banksajt/backend/src/index.ts
+++ V9/node-express-banksajt/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { User, Account, Session } from "./types";
+import mysql from "mysql2/promise";
 
 const users: User[] = [];
 
@@ -27,36 +28,69 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.post("/users", (req, res) => {
-  const { user, password } = req.body;
-  users.push({ username: user, password: password });
-  res.send("SEND OK")
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+  const result = await query(
+    "INSERT INTO users (username, password) VALUES (?, ?)",
+    [username, password]
+  );
+  res.send(result);
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+     const users = await query("SELECT * FROM users", "");
   res.send(users);
 });
 
 app.post("/accounts", (req, res) => {
-  const { user, amount } = req.body;
-  accounts.push({ username: user, amount: amount });
-  res.send("SEND OK")
+  const {amount, token } = req.body;
+  const foundSession = sessions.find((session) => session.token === token) 
+  if (foundSession)
+    {
+    accounts.push({ username: foundSession.username, amount: amount });
+    res.status(200).send(accounts)
+  } 
+  else res.status(401).send("Token or username not found")
 });
 
 app.get("/accounts", (req, res) => {
-  res.send(accounts);
+  const { token } = req.body
+  const foundSession = sessions.find((session) => session.token === token) 
+  if (foundSession)
+    {
+      const foundAccount = accounts.find((account => account.username === foundSession.username))
+      res.send(foundAccount?.amount.toString());
+  }
+  res.send("No token found")
 });
 
 app.post("/sessions", (req, res) => {
   const { username, password } = req.body;
   if (users.find((user) => user.username === username)?.password === password) {
-    sessions.push({ username: username, token: generateOTP() });
+    const generatedToken = generateOTP() 
+    console.log(generatedToken)
+    sessions.push({ username: username, token: generatedToken });
+    res.status(200).send(generatedToken)
   } 
-  res.send("SEND OK")
+  res.status(401).send("Incorrect username or password")
 });
 
 app.get("/sessions", (req, res) => {
-  res.send(sessions);
+  res.send(sessions)
 });
 
+// Databas uppkoppling
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "banksajt",
+  port: 3306, // Obs! 3306 för windowsanvändare
+});
+
+// Funktion för att göra förfrågan till databas
+async function query(sql: any, params: any) {
+  const [results] = await pool.execute(sql, params);
+  return results;
+}
 
